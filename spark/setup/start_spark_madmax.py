c70cb6aab79e2820bb9ed125a3f30156db4b084f
Peter Steinbach <steinbach@scionics.de>
2015-09-17 14:44:30 +0200

    switched from using mpirun to plain ssh

--- spark/setup/start_spark_madmax.py
+++ spark/setup/start_spark_madmax.py
@@ -1,5 +1,8 @@
 #! /usr/bin/python
-import os, socket, sys, re
+import os
+import sys
+import re
+import subprocess
 
 spark_dir = os.getenv('SPARK_HOME')
 if not spark_dir:
@@ -8,22 +11,17 @@ spark_sbin = spark_dir + '/sbin'
 
 # figure out which hosts we have
 pat = re.compile('[n]\d+')
-hosts = pat.findall(os.environ.get('LSB_MCPU_HOSTS'))
+lsb_mcpu_hosts = os.getenv('LSB_MCPU_HOSTS')
+hosts = pat.findall(lsb_mcpu_hosts)
 
-print ">> LSB_MCPU_HOSTS",os.getenv('LSB_MCPU_HOSTS')
+print ">> LSB_MCPU_HOSTS",lsb_mcpu_hosts
 print ">> found hosts: ", hosts
 
-# main host
-my_host = socket.gethostname()
-max_cores_per_slave = 12
+master_ip = os.getenv("HOSTNAME")
 
 # set up slave file
 slave_file = '%s/slaves_%s' % (os.getenv('HOME'),os.getenv('LSB_JOBID'))
 
-# set up the slaves file
-with open(slave_file,'w') as slaves:
-    for host in hosts :
-        slaves.write("%s\n"%host)
 
 if  __name__ == "__main__":
     import getopt
@@ -41,12 +39,17 @@ if  __name__ == "__main__":
     for opt, arg in opts :
         if opt == '-m' :
             mem = arg
+        if opt == '-s' :
+            if os.path.exists(arg) and os.path.is_directory(arg):
+                slave_file = '%s/slaves_%s' % (arg,os.getenv('LSB_JOBID'))
+                os.environ['SPARK_LOCAL_DIRS']=arg
 
-        if opt == '-c' :     # set to 12 if sufficient memory
-            cores = arg
-	else : 
-	    cores = 12
+    # set up the slaves file
+    with open(slave_file,'w') as slaves:
+        for host in hosts :
+            slaves.write("%s\n"%host)
 
+            
     if mem is not None:
         os.environ['SPARK_EXECUTOR_MEMORY'] = mem.upper()
 
@@ -54,22 +57,33 @@ if  __name__ == "__main__":
 
     master_command = '%s/start-master.sh' % spark_sbin
 
-    n_slaves_used = len(hosts)
-
-    #http://stackoverflow.com/questions/28216897/syntax-of-the-map-by-option-in-openmpi-mpirun-v1-8
-    slaves_command = 'mpirun --map-by ppr:1:node %s/start-slave.sh spark://%s:7077' % (#int(cores),
-        spark_sbin,
-        my_host)
-    
-    if mem:
-        slaves_command += " -m %s" % (mem.upper())
-
-    slaves_command += " &"
-        
-    print slaves_command
-
-    # start the host and slaves
+    # start the master
     print 'master command ', master_command
-    print 'slaves command ', slaves_command
-    os.system(master_command)
-    os.system(slaves_command)
+    mc = subprocess.check_output(master_command.split())
+    print mc
+    print "master set to ",master_ip
+    
+    
+    #http://stackoverflow.com/questions/28216897/syntax-of-the-map-by-option-in-openmpi-mpirun-v1-8
+    # slaves_command = 'mpirun -np %i %s/start-slave.sh spark://%s:7077 -c 1' % (int(cores),
+    #                                                                            spark_sbin,
+    #                                                                            master_ip)
+
+    #start the slaves/workers
+    n_slaves_used = len(hosts)    
+    hnc_pat = re.compile('[n]\d+ \d+')
+    hosts_and_cores = hnc_pat.findall(lsb_mcpu_hosts)
+    cnt = 0
+    for t in hosts_and_cores:
+        node,recv_cores = t.split()
+        cmd = "ssh %s %s/start-slave.sh  -c %i" % (node,spark_sbin,int(recv_cores))
+        if mem:
+            cmd += " -m %s" % (mem.upper())
+        cmd += " spark://%s:7077 &" % master_ip
+        output = subprocess.check_output(cmd.split())
+        print ">> slave %i/%i\t%s" % (cnt,n_slaves_used,cmd)
+        print output
+        cnt+=1
+
+
+
