ffce81db41716b89cb866850f28be6f888969a23
Peter Steinbach <steinbach@scionics.de>
2015-09-24 11:29:52 +0200

    reordered spark directory

--- python/bootstrap_utils.py
+++ python/bootstrap_utils.py
@@ -19,15 +19,20 @@ def random_rotate(_image, _seed = 0, _verbose=False):
     return rotate(_image,degrees)
 
 
-def random_enhance_color(_image, _seed = 0, _color_id = -1):
+def random_enhance_color(_image, _verbose=False, _seed = 0, _color_id = -1):
     """ adds random value from [0,256) to random channel of _image, 
     if _color_id is -1, the channel is also chosen by a random number  """
     
     random.seed(_seed)
+    if not _seed:
+        random.seed(time.time())
+
     if _color_id < 0 or _color_id > 2:
         _color_id = random.randrange(3)
 
     delta = random.randrange(0,255)
+    if _verbose:
+        print "enhancing channel %i by %i ticks" %(_color_id,delta)
 
     value = np.copy(_image)
     cvals = value[...,_color_id]
@@ -43,7 +48,7 @@ def change_and_write(_fname,_image,_args):
         value = random_rotate(value,0,_args.verbose)
         
     if not _args.rotate_only:    
-        value = random_enhance_color(value)
+        value = random_enhance_color(value, _args.verbose)
 
     #write to disk
     namebase,nameext = os.path.splitext(_fname)
