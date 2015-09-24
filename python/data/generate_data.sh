bb399ff7d9543eef16213868a2c65a83ffb73dfd
Peter Steinbach <steinbach@scionics.de>
2015-09-24 21:06:25 +0200

    added standalone data

---
+++ python/data/generate_data.sh
@@ -0,0 +1,19 @@
+echo "id n_cores n_files start_epoch end_epoch duration_s" > temp.data
+N_FILES=296
+NCORES=12
+
+for f in *log;do 
+    
+    ID=${f/.log/}
+
+    STARTDATE=`egrep -i '^Started' ${f}|sed -e 's/Started at//'`;
+    ENDDATE=`egrep -i '^Results reported at' ${f}|sed -e 's/Results reported at//'`;
+
+    START_EPOCH=`date -d "${STARTDATE}" +%s`
+    END_EPOCH=`date -d "${ENDDATE}" +%s`
+
+    DELTA=`echo "${END_EPOCH}-${START_EPOCH}"|bc`
+    echo -e "${ID} ${NCORES} ${N_FILES} ${START_EPOCH} ${END_EPOCH} ${DELTA}";
+done|column -t >> temp.data
+cat temp.data | column -t > standalone.data
+rm temp.data
---
+++ spark/data/generate_data.sh
@@ -0,0 +1,18 @@
+echo "id n_cores n_files start_epoch end_epoch duration_s" > temp.data
+N_FILES=296
+
+for f in *log;do 
+    
+    ID=${f/.log/}
+    NCORES=`echo $f|cut -f3 -d'_'`
+    STARTDATE=`egrep -i '^Started' ${f}|sed -e 's/Started at//'`;
+    ENDDATE=`egrep -i '^Results reported at' ${f}|sed -e 's/Results reported at//'`;
+
+    START_EPOCH=`date -d "${STARTDATE}" +%s`
+    END_EPOCH=`date -d "${ENDDATE}" +%s`
+
+    DELTA=`echo "${END_EPOCH}-${START_EPOCH}"|bc`
+    echo -e "${ID} ${NCORES} ${N_FILES} ${START_EPOCH} ${END_EPOCH} ${DELTA}";
+done|column -t >> temp.data
+cat temp.data | column -t > spark_gpu.data
+rm temp.data
