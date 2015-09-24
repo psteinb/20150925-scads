920ed61baaebeb1b96e61499e3171ce330799c1e
Peter Steinbach <steinbach@scionics.de>
2015-09-24 22:00:43 +0200

    first version of plot done

--- slides/plots/plot.R
+++ slides/plots/plot.R
@@ -12,13 +12,15 @@ if(length(argv) > 1){
 standalonefile <- argv[2]
 } 
 
-spark_data <- read.table(sparkfile, header = T,stringsAsFactors=TRUE)
 standalone_data <- read.table(standalonefile, header = T,stringsAsFactors=TRUE)
 
-library(ggplot2)
-library(dplyr)
+spark_data <- read.table(sparkfile, header = T,stringsAsFactors=TRUE)
+spark_data$speedup <- mean(standalone_data$duration_s) / spark_data$duration_s 
 
-filtered_data <- filter(bench_data, id != "_lrt")
+library(ggplot2)
+## library(dplyr)
+
+## filtered_data <- filter(bench_data, id != "_lrt")
 
 my_theme <-  theme_bw() + theme(axis.title.x = element_text(size=20),
                                 axis.title.y = element_text(size=20),
@@ -33,23 +35,29 @@ my_theme <- my_theme + theme(legend.title = element_text(size=22, face="bold"),
                              legend.position="top",
 #                             legend.position=c(0, 1.),
                              legend.box.just="left",
-                             legend.margin=unit(1, "pt"),
+                             
 #                             legend.justification=c(0,0),
                              legend.key = element_rect(colour = 'white', fill = 'white', size = 0., linetype='dashed')) #+ theme(legend.title=element_blank()) 
 
 
-mse_plot <- ggplot(filtered_data, aes(x=bw_mb_p_sec,
-                                      y=compression_ratio,
-                                      size=mse,
-                                      shape=literal_id,
-                                      color=brieffname)) 
-mse_plot <- mse_plot + geom_point() + ylim(75,175)
-mse_plot <- mse_plot + ylab("raw_size/compressed_size") + xlab("ingest bandwidth [MB/s]") + guides(col=guide_legend(ncol=3))
-mse_plot <- mse_plot + guides(color=FALSE) + scale_shape_discrete(name="Type") 
-mse_plot <- mse_plot + my_theme #+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+standalone_plot <- ggplot(standalone_data, aes(x=n_cores,
+                                      y=duration_s)) 
+standalone_plot <- standalone_plot + geom_point() 
+standalone_plot <- standalone_plot + ylab("runtime / s") + xlab("N_cores") 
+standalone_plot <- standalone_plot + my_theme #+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+
+ggsave("spark_standalone.png",standalone_plot)
+ggsave("spark_standalone.svg",standalone_plot)
 
 
-ggsave("spark_scale_",mse_plot)
-ggsave("spark_scale_",mse_plot)
-ggsave("spark_scale_",mse_plot)
+spark_plot <- ggplot(spark_data, aes(x=factor(n_cores),
+                                      y=max(speedup))) 
+spark_plot <- spark_plot + geom_point() #+ geom_point(data=standalone_data,aes(y=duration_s),color=2)
+spark_plot <- spark_plot + ylab("speed up") + xlab("Cores") 
+spark_plot <- spark_plot + my_theme #+  theme(panel.grid.major = element_blank(),panel.grid.minor = element_blank())
+spark_plot <- spark_plot + geom_hline(data=standalone_data,aes(yintercept=1),   # Ignore NA values for mean
+               color="red", linetype="dashed", size=1)
+
+ggsave("spark_scaling.png",spark_plot)
+ggsave("spark_scaling.svg",spark_plot)
 
